39609a702fc13def50f331743e953d79e3e2dfec
vips120 <vipulsingh0000@gmail.com>
2020-03-14 17:00:53 +0530

    rxjs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { fromEvent } from "rxjs";
+import { map,filter,debounceTime } from "rxjs/operators";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +11,17 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let search = document.getElementById("search");
+    let observer = fromEvent(search, "keyup")
+      .pipe(map((e: any) => e.target.value),
+        filter((text) => text.length >= 4),
+          debounceTime(2000)   
+    )      
+      ;
+    observer.subscribe(data => {
+      console.log(data);
+    })
+
   }
 
 }
